a00f4ddb2027904648b12a58fa41d18f32a132ae
Mannie004 <seun.adeyemi04@yahoo.com>
2022-05-03 17:57:54 -0400

    added new BMI chart

--- BMI/script.js
+++ BMI/script.js
@@ -19,9 +19,9 @@ function calculateBMI() {
   // Checking the user providing a proper
   // value or not
   if (height === "" || isNaN(height))
-    result.innerHTML = "Provide a valid Height!";
+    result.innerHTML = "Please provide a valid Height!";
   else if (weight === "" || isNaN(weight))
-    result.innerHTML = "Provide a valid Weight!";
+    result.innerHTML = "Please provide a valid Weight!";
   // If both input is valid, calculate the bmi
   else {
     // Fixing upto 2 decimal places
